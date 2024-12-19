20df7f27b1d75358156c2e5545b570d6e4843f5f
jihyewlgP <jh083314@naver.com>
2024-12-19 10:59:37 +0900

    visualization add

--- 크롭이미지예측참조/detect.py
+++ 크롭이미지예측참조/detect.py
@@ -14,6 +14,7 @@ from efficientnet_pytorch.model import EfficientNet
 from preprocessor.image_preprocess import auto_adjustments, opencv_processing
 from config import PATH, MODEL
 from crop_image import image_crop
+from visualization import img_indication
 from collections import defaultdict
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,6 +23,9 @@ class Detect:
     def __init__(self, input_dir, output_dir):
         self.input_dir = input_dir
         self.output_dir = output_dir
+        self.ok_dir = os.path.join(self.output_dir, 'OK')
+        self.pass_dir = os.path.join(self.output_dir, 'PASS')
+        self.ng_dir = os.path.join(self.output_dir, 'NG')
 
         self.model_name = MODEL['MODEL_NAME']
         self.weights_dir = PATH['WEIGHTS_PATH']
@@ -57,12 +61,6 @@ class Detect:
         # 이미지 파일 리스트 : ['MFR01328AE;2408221189A_R_OK.png', 'MFR01327AE;2409110702A_R_NG.png', ....]
         img_files = [f for f in os.listdir(self.input_dir) if f.endswith(('.jpg', '.png')) and f not in img_list]
         imagelist, filenamelist, croplist = image_crop(self.input_dir) # 크롭된 PIL 이미지, 파일명 호출
-        # img_files = [f for f in filenamelist if f not in img_list] 
-
-        font_size = 15
-        font = ImageFont.truetype("arial.ttf", font_size)
-        # 라벨 우선순위 정의
-        label_priority = {'NG': 1, 'PASS': 2, 'OK': 3}
 
         # 파일명 기준 라벨 그룹화
         label_map = defaultdict(list)
@@ -98,59 +96,7 @@ class Detect:
             except Exception as e:
                 logging.info(f'{img} : {e}')
 
-        ok_dir = os.path.join(self.output_dir, 'OK')
-        pass_dir = os.path.join(self.output_dir, 'PASS')
-        ng_dir = os.path.join(self.output_dir, 'NG')
-
-        # 우선순위에 따라 최종 라벨 결정 및 파일 복사
-        for filename, labels in label_map.items():
-            # 원본 파일 경로 찾기
-            img_file = next((file for file in img_files if filename in file), None)
-            if img_file:
-                img_path = os.path.join(input_dir, img_file)
-
-                # 이미지 열기
-                with Image.open(img_path) as img:
-                    draw = ImageDraw.Draw(img)
-                    
-                    # 좌표를 하나씩 그리기
-                    for label_entry in labels:
-                        coords = label_entry['coords']  # (y, x, h, w)
-                        label = label_entry['label']
-                        
-                        if label in ['NG', 'PASS']:
-                            # 좌표 변환
-                            y, x, h, w = coords
-                            
-                            # 박스 그리기
-                            box_color = "red" if label == 'NG' else "green"
-                            draw.rectangle((x, y, x+w, y+h), outline = box_color, width = 15)
-
-                            text_pos = (x+70, y+50)
-                            #font = ImageFont.load_default() #폰트 사이즈 변경이 안됨
-                            font_path = "C:/Windows/Fonts/arial.ttf"  # TTF 폰트 파일 경로
-                            font_size = 100
-                            font = ImageFont.truetype(font_path, font_size)
-                            
-                            draw.text(text_pos, label, fill = box_color, font=font)
-                    
-                    # 최종 라벨 결정
-                    final_label_entry = min(labels, key=lambda x: label_priority[x['label']])
-                    final_label = final_label_entry['label']
-                    
-                    # 복사 대상 디렉토리 선택
-                    if final_label == 'OK': 
-                        target_dir = ok_dir 
-                    elif final_label == 'PASS':
-                        target_dir = pass_dir
-                    else:  # final_label == 'NG'
-                        target_dir = ng_dir
-                    
-                    # 저장 경로
-                    target_path = os.path.join(target_dir, img_file)
-                    
-                    # 표시된 이미지 저장
-                    img.save(target_path)
+        img_indication(self, img_files, label_map)
 
 if __name__ == '__main__':
     import sys
---
+++ 크롭이미지예측참조/visualization.py
@@ -0,0 +1,53 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+# 우선순위에 따라 최종 라벨 결정 및 파일 복사
+def img_indication(self, img_files, label_map):
+    # 라벨 우선순위 정의
+    label_priority = {'NG': 1, 'PASS': 2, 'OK': 3}
+    
+    font_path = "C:/Windows/Fonts/arial.ttf"  # TTF 폰트 파일 경로
+    font_size = 100
+    font = ImageFont.truetype(font_path, font_size)
+
+    for filename, labels in label_map.items():
+        # 원본 파일 경로 찾기
+        img_file = next((file for file in img_files if filename in file), None)
+        if img_file:
+            img_path = os.path.join(self.input_dir, img_file)
+
+        # 이미지 열기
+        with Image.open(img_path) as img:
+            draw = ImageDraw.Draw(img)
+        
+            # 좌표를 하나씩 그리기
+            for label_entry in labels:
+                coords = label_entry['coords']  # (y, x, h, w)
+                label = label_entry['label']
+                
+                if label in ['NG', 'PASS']:
+                    # 좌표 변환
+                    y, x, h, w = coords
+                    
+                    # 박스 그리기
+                    box_color = "red" if label == 'NG' else "green"
+                    draw.rectangle((x, y, x+w, y+h), outline = box_color, width = 15)
+                    draw.text((x+70, y+50), label, fill = box_color, font=font)
+
+            # 최종 라벨 결정
+            final_label_entry = min(labels, key=lambda x: label_priority[x['label']])
+            final_label = final_label_entry['label']
+
+            # 복사 대상 디렉토리 선택
+            if final_label == 'OK': 
+                target_dir = self.ok_dir 
+            elif final_label == 'PASS':
+                target_dir = self.pass_dir
+            else:
+                target_dir = self.ng_dir
+
+            # 저장 경로
+            target_path = os.path.join(target_dir, img_file)
+
+            # 표시된 이미지 저장
+            img.save(target_path)
